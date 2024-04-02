bfb1fbdb8f573688694fc5f23b857e6cd268da77
alekhya-korsa2612 <150769034+alekhya-korsa2612@users.noreply.github.com>
2024-04-02 15:04:47 +0530

    Add files via upload

---
+++ model/authModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: { type: String, unique: true },
+    email: { type: String, unique: true },
+    mobile: { type: Number, unique: true },
+    password: { type: String, required: true },
+});
+
+module.exports = mongoose.model('User', userSchema);
+
+
+
